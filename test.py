36accc77e3a9a77b136529d1ad6719e287b73a94
Emanuel Bolanos <180501265+bolanosmanny@users.noreply.github.com>
2026-06-23 13:38:18 -0400

    Refactor test.py to include calculate_stuff function
    Added a function to calculate stuff with a nested loop.

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 #TODO: Fix this bug later on when we have time
 print("Hello World!!")
+
+# Messy test code
+def calculate_stuff(x):
+    # Unnecessary nested loop
+    for i in range(10):
+        for j in range(10):
+            print(     "formatting issue here"   )
+    return x * 2
